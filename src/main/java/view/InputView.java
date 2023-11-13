7e9cf84f34d66a6cccb4d65bc690695a02126646
Picka <marmong@naver.com>
2023-11-14 02:08:32 +0900

    refactor: add checkDate for validateDate

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,15 +1,16 @@
 package view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.HashMap;
 import org.junit.jupiter.params.aggregator.ArgumentAccessException;
 
 public class InputView {
-    private static String errorMessage = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    private static String dateMessage = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static String ERRORMESSAGE = "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
+    private static String DATEMESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
 
-    private static String menuMessage = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    private static String MENUMESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
 
-    public String getInput() {
+    private String getInput() {
         return Console.readLine();
     }
 
@@ -18,7 +19,7 @@ public class InputView {
         String input = "";
         int reservationDate = 0;
         while (!validate) {
-            System.out.println(dateMessage);
+            System.out.println(DATEMESSAGE);
             input = getInput();
             validate = validateDate(input);
         }
@@ -26,12 +27,12 @@ public class InputView {
         return reservationDate;
     }
 
-    public String menuInputter() {
+    public HashMap<String, Integer> menuInputter() {
         boolean validate = false;
         String input = "";
-        String orderedMenu = "";
+        HashMap<String, Integer> orderedMenu = new HashMap<>();
         while (!validate) {
-            System.out.println(menuMessage);
+            System.out.println(MENUMESSAGE);
             input = getInput();
             validate = validateMenu(input);
         }
@@ -42,24 +43,27 @@ public class InputView {
     private boolean validateDate(String input) {
         try {
             int reservationDate = Integer.parseInt(input);
-            if (reservationDate <= 0 || reservationDate > 31) {
-                System.out.println(errorMessage);
-                return false;
-            }
-            return true;
+            return checkDate(reservationDate);
         } catch (NumberFormatException e) {
-            System.out.println(errorMessage);
+            System.out.println(ERRORMESSAGE);
         }
         return false;
     }
 
-    private boolean validateMenu(String input){
-        try{
-
-        }catch (ArgumentAccessException e){
-
+    private boolean checkDate(int date){
+        if (date <= 0 || date > 31) {
+            System.out.println(ERRORMESSAGE);
+            return false;
         }
+        return true;
     }
 
+    private boolean validateMenu(String input) {
+        return false;
+    }
+
+    private boolean duplicateCheck(String name) {
+        return false;
+    }
 
 }
