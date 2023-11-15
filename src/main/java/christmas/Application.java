fba8733e75ca07e589abf3357ca6d605c8bf5b95
Picka <marmong@naver.com>
2023-11-15 18:56:19 +0900

    feat: implement apllication logic

--- src/main/java/christmas/Application.java
+++ src/main/java/christmas/Application.java
@@ -1,7 +1,23 @@
 package christmas;
 
+import dto.Receipt;
+import model.CalculatorTotalCost;
+import view.InputView;
+import view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        outputView.greetMessageOutputter();
+        Receipt receipt = new Receipt(inputView.dateInputter(), inputView.menuInputter());
+        outputView.benefitsPreviewOutputter(receipt);
+        outputView.totalOrderOutputter(receipt);
+        outputView.totalAmountBeforeDiscountOutputter(receipt);
+        outputView.giveAwayOutputter(receipt);
+        outputView.benefitDetailsOutputter(receipt);
+        outputView.totalBenefitAmountOutputter(receipt);
+        outputView.estimatedPaymentAmountAfterDiscount(receipt);
+        outputView.BadgeOutputter(receipt);
     }
 }
