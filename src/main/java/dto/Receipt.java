ed896fa39dd0c0c599bd591abdfe3a33dcf3bec1
Picka <marmong@naver.com>
2023-11-15 18:17:11 +0900

    feat: add getter kind of discounts to Receipt

--- src/main/java/dto/Receipt.java
+++ src/main/java/dto/Receipt.java
@@ -2,7 +2,9 @@ package dto;
 
 import java.util.HashMap;
 import model.CalculatorTotalCost;
+import model.Menu.Beverage;
 import model.Menu.Menu;
+
 
 public class Receipt {
     private final int month = 12;
@@ -10,30 +12,70 @@ public class Receipt {
     private final HashMap<String, Integer> orders;
     private final int totalAmountBeforeDiscount;
 
+    private  int totalDiscountAmount = 0;
+
+    private  int weekendDiscountAmount = 0;
+    private  int weekdayDiscountAmount = 0;
+    private  int eventdayDiscountAmount = 0;
+    private  int chirstmasDDayDiscountAmount = 0;
+
+    private  int giveAwayAmount;
+
     private final HashMap<String, Integer> giveAway;
 
     public Receipt(int date, HashMap<String, Integer> orders) {
         this.date = date;
         this.orders = orders;
-        CalculatorTotalCost calculatorTotalCost = new CalculatorTotalCost(orders , date);
+        CalculatorTotalCost calculatorTotalCost = new CalculatorTotalCost(orders, date);
         this.totalAmountBeforeDiscount = calculatorTotalCost.getTotalAmountBeforeDiscount();
         this.giveAway = ReceiveGiveaway();
+        if (totalAmountBeforeDiscount > 10000) {
+            this.totalDiscountAmount = calculatorTotalCost.calculateTotalDiscount();
+            this.weekendDiscountAmount = calculatorTotalCost.calculateWeekendDiscount();
+            this.weekdayDiscountAmount = calculatorTotalCost.calculateWeekdayDiscount();
+            this.eventdayDiscountAmount = calculatorTotalCost.calculateEventDayDiscount();
+            this.chirstmasDDayDiscountAmount = calculatorTotalCost.calculateChristmasDDayDiscount();
+            this.giveAwayAmount = getGiveAwayAmount();
+        }
     }
 
     public HashMap<String, Integer> getGiveAway() {
         return giveAway;
     }
 
+    public int getGiveAwayAmount() {
+        for (String key : giveAway.keySet()
+        ) {
+            return Menu.getMenuPrice(key);
+        }
+        return 0;
+    }
+
     public int getTotalAmountBeforeDiscount() {
         return totalAmountBeforeDiscount;
     }
 
-    public int getMonth() {
-        return month;
+
+    public int getTotalDiscountAmount() {
+        return totalDiscountAmount ;
     }
 
-    public int getYear(){
-        return 2023;
+
+
+    public int getWeekendDiscountAmount() {
+        return weekendDiscountAmount;
+    }
+
+    public int getWeekdayDiscountAmount() {
+        return weekdayDiscountAmount;
+    }
+
+    public int getEventdayDiscountAmount() {
+        return eventdayDiscountAmount;
+    }
+
+    public int getChirstmasDDayDiscountAmount() {
+        return chirstmasDDayDiscountAmount;
     }
 
     public int getDate() {
@@ -49,7 +91,7 @@ public class Receipt {
         HashMap<String, Integer> item = new HashMap<>();
         if (totalAmountBeforeDiscount > 120000) {
             int count = 1;
-            item.put("삼페인", count);
+            item.put("샴페인", count);
         }
         return item;
     }
