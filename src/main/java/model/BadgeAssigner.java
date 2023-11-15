5ae6b3c8b3f82a0874fa064294d162ab3eee4bfd
Picka <marmong@naver.com>
2023-11-15 18:18:19 +0900

    feat: implement BadgeAssingner

---
+++ src/main/java/model/BadgeAssigner.java
@@ -0,0 +1,27 @@
+package model;
+
+import dto.Receipt;
+
+public class BadgeAssigner {
+    private final String badge;
+    private final String STAR = "별";
+    private final String TREE = "트리";
+    private final String SANTA = "산타";
+
+    public BadgeAssigner(Receipt receipt) {
+        int totalBenefitsAmount = receipt.getTotalDiscountAmount() + receipt.getGiveAwayAmount();
+
+        if (totalBenefitsAmount >= 20000) {
+            this.badge = SANTA;
+        } else if (totalBenefitsAmount >= 10000) {
+            this.badge = TREE;
+        } else if (totalBenefitsAmount >= 5000) {
+            this.badge = STAR;
+        } else {
+            this.badge = null; // NULL 대신 null 사용
+        }
+    }
+    public String getBadge() {
+        return badge;
+    }
+}
