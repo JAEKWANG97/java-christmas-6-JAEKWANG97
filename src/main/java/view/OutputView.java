d088dd42b5e1a1f4053c36f58bc1171d6afc09f1
Picka <marmong@naver.com>
2023-11-15 10:35:45 +0900

    feat: add totalOrderOutputter to OutputView

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,10 +1,23 @@
 package view;
 
+import dto.Receipt;
+import java.util.HashMap;
+import model.Menu.Menu;
+
 public class OutputView {
     public void greetMessageOutputter(){
         System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.");
     }
     public void benefitsPreviewOutputter(){
         System.out.println("12월 26일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+
+    public void totalOrderOutputter(Receipt receipt){
+        HashMap<String, Integer> orders = receipt.getOrders();
+        System.out.println("<주문 메뉴>");
+        for (String key : orders.keySet()) {
+            Integer value = orders.get(key);
+            System.out.println(key + " "+ value+"개");
+        }
     }
 }
