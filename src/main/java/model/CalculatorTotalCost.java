d8fd6a7b42f9ff321d6a74dc30b355ca71764dc8
Picka <marmong@naver.com>
2023-11-15 13:56:27 +0900

    feat: add caculate weekend or weekday discount to CalculatorTotalCost

--- src/main/java/model/CalculatorTotalCost.java
+++ src/main/java/model/CalculatorTotalCost.java
@@ -1,29 +1,40 @@
 package model;
 
+import static model.Menu.Menu.getDessertPrice;
+import static model.Menu.Menu.getMainDishtPrice;
+
 import dto.Receipt;
 import java.time.format.TextStyle;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Locale;
 import model.Menu.Menu;
-import java.time.DayOfWeek;import java.time.LocalDate;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
 public class CalculatorTotalCost {
     private HashMap<String, Integer> receipt;
     private int date;
     private final int totalAmountBeforeDiscount;
 
+    private final int discountPrice = 2023;
 
-    public CalculatorTotalCost(HashMap<String, Integer> receipt , int date) {
+    private final String[] weekday = {"Sunday" , "Monday" , "Tuesday" , "Wednesday" , "Thursday"};
+    private final String[] weekend = {"Friday","Saturday"};
+    private final int[] eventDay = {3,10,17,24,31};
+
+
+
+    public CalculatorTotalCost(HashMap<String, Integer> receipt, int date) {
         this.receipt = receipt;
         this.date = date;
         this.totalAmountBeforeDiscount = calculateTotalAmountBeforeDiscount(receipt);
 
     }
 
-    public int getTotalAmountBeforeDiscount(){
+    public int getTotalAmountBeforeDiscount() {
         return totalAmountBeforeDiscount;
     }
-
 
 
     private int calculateTotalAmountBeforeDiscount(HashMap<String, Integer> receipt) {
@@ -35,24 +46,59 @@ public class CalculatorTotalCost {
         return cost;
     }
 
-    private String getReservationDay(int day){
+    private String calculateReservationDay(int day) {
         LocalDate date = LocalDate.of(2023, 12, day);
-        String dayOfWeek = date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
-        return dayOfWeek;
+        return date.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.ENGLISH);
     }
 
     //평일 할인
-    private int calculateChristmasDDayDiscount(int date){
-        int discount = 1000;
-        if (date <= 25){
-            return discount + 100 * (24);
+
+
+    public int calculateDayDiscount(int date){
+        int discount = 0;
+        String day = calculateReservationDay(date);
+        if (Arrays.asList(weekday).contains(day)){
+            discount = calculateWeekdayDiscount();
+        }
+        if (Arrays.asList(weekend).contains(date)){
+            discount = calculateWeekendDiscount();
         }
         return discount;
+    }
+
+    public int calculateChristmasDDayDiscount() {
+        int discount = 1000;
+        if (date <= 25) {
+            return discount + 100 * (date - 1);
+        }
+        return discount;
+    }
+
+    private int calculateWeekdayDiscount(){
+        int discount = 0;
+        for (String key : receipt.keySet()) {
+            Integer value = receipt.get(key);
+            discount += getDessertPrice(key);
+        }
+        return discount;
+    }
+
+    private int calculateWeekendDiscount(){
+        int discount = 0;
+        for (String key : receipt.keySet()) {
+            Integer value = receipt.get(key);
+            discount += getMainDishtPrice(key);
+        }
+        return discount;
+    }
+
+    private int calculateEventDayDiscount(){
+        return 1000;
     }
 
     //평일 할인(일요일~목요일): 평일에는 디트저 메뉴를 메뉴 1개당 2,023원 할인
     //주말 할인(금요일, 토요일): 주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인
     //특별 할인: 이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인
-    //이벤트 기간: '크리스마스 디데이 할인'을 제외한 다른 이벤트는 2023.12.1 ~ 2023.12.31 동안 적용
+    //[x] 이벤트 기간: '크리스마스 디데이 할인'을 제외한 다른 이벤트는 2023.12.1 ~ 2023.12.31 동안 적용
 
 }
